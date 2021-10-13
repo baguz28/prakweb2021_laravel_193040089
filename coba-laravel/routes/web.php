20d642e5b10e2383b8697dfa92553d77f46a246e
baguz28 <bagussyahdewa28@gmail.com>
2021-10-13 14:11:46 +0700

    struktur folder, views dan routes

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Tito Bagus",
+        "email" => "baguz@mail",
+        "image" => "logo.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });

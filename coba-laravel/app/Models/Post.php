5ca572572c578251c9692fd52a50871535084e31
baguz28 <bagussyahdewa28@gmail.com>
2021-10-21 13:25:54 +0700

    post-model

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -2,34 +2,13 @@
 
 namespace App\Models;
 
-use function PHPUnit\Framework\returnSelf;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post 
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Tito Bagus",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque corrupti perspiciatis aliquid fuga praesentium atque molestiae accusantium, voluptatibus cum aperiam assumenda tempore possimus quas quasi modi corporis recusandae exercitationem? Aperiam a nulla impedit dolores placeat recusandae quisquam ipsam doloremque, sapiente perferendis cupiditate eligendi aliquam odit incidunt! Suscipit commodi sapiente accusantium harum quis atque laborum quasi quidem illum sed accusamus minima ut inventore laudantium eos unde, esse tenetur fugit ab vel saepe fuga modi rerum. Laudantium ea sint eveniet eligendi placeat."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Syahdewa",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque corrupti perspiciatis aliquid fuga praesentium atque molestiae accusantium, voluptatibus cum aperiam assumenda tempore possimus quas quasi modi corporis recusandae exercitationem? Aperiam a nulla impedit dolores placeat recusandae quisquam ipsam doloremque, sapiente perferendis cupiditate eligendi aliquam odit incidunt! Suscipit commodi sapiente accusantium harum quis atque laborum quasi quidem illum sed accusamus minima ut inventore laudantium eos unde, esse tenetur fugit ab vel saepe fuga modi rerum. Laudantium ea sint eveniet eligendi placeat."
-        ],
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
-
+    //protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }

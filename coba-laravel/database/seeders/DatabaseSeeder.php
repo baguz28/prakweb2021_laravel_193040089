dcc97eaf5b4c4f42699cc37876e5b4704ebc118c
baguz28 <bagussyahdewa28@gmail.com>
2021-10-25 12:43:43 +0700

    database seeder

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,64 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //\App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Tito Bagus',
+            'email' => 'tito@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Abel',
+            'email' => 'abel@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem, ipsum pertama',
+            'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore laudantium maxime veritatis vero, quaerat necessitatibus ratione voluptatum quod eveniet id, perferendis iure odit rem quis cum molestiae eaque quam sapiente adipisci ipsa dolore voluptas facere ipsum. Atque nihil vitae possimus esse, quis dignissimos sequi eaque modi officiis eligendi sunt in accusamus. Itaque ut nisi laborum magni labore blanditiis? Assumenda soluta amet iusto! Nisi aspernatur illo obcaecati et iusto unde corporis exercitationem commodi amet, sunt voluptates soluta eaque omnis eveniet similique magni quae cumque id accusantium vel minima nobis est esse?</p> <p>Aspernatur, ullam perspiciatis aliquid qui quos nam expedita et recusandae eaque quae iste. Molestiae, quos quia! Officia autem non sapiente ullam architecto, tempora nisi ex temporibus laboriosam sed perspiciatis et impedit quasi repellat voluptatem repellendus. Impedit in molestiae excepturi, fugit cupiditate ex? Quaerat pariatur, eum maxime veritatis ipsum voluptate consequuntur id repudiandae odio nulla consequatur sed culpa, fugiat expedita tempora.</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem, ipsum kedua',
+            'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore laudantium maxime veritatis vero, quaerat necessitatibus ratione voluptatum quod eveniet id, perferendis iure odit rem quis cum molestiae eaque quam sapiente adipisci ipsa dolore voluptas facere ipsum. Atque nihil vitae possimus esse, quis dignissimos sequi eaque modi officiis eligendi sunt in accusamus. Itaque ut nisi laborum magni labore blanditiis? Assumenda soluta amet iusto! Nisi aspernatur illo obcaecati et iusto unde corporis exercitationem commodi amet, sunt voluptates soluta eaque omnis eveniet similique magni quae cumque id accusantium vel minima nobis est esse?</p> <p>Aspernatur, ullam perspiciatis aliquid qui quos nam expedita et recusandae eaque quae iste. Molestiae, quos quia! Officia autem non sapiente ullam architecto, tempora nisi ex temporibus laboriosam sed perspiciatis et impedit quasi repellat voluptatem repellendus. Impedit in molestiae excepturi, fugit cupiditate ex? Quaerat pariatur, eum maxime veritatis ipsum voluptate consequuntur id repudiandae odio nulla consequatur sed culpa, fugiat expedita tempora.</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem, ipsum ketiga',
+            'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore laudantium maxime veritatis vero, quaerat necessitatibus ratione voluptatum quod eveniet id, perferendis iure odit rem quis cum molestiae eaque quam sapiente adipisci ipsa dolore voluptas facere ipsum. Atque nihil vitae possimus esse, quis dignissimos sequi eaque modi officiis eligendi sunt in accusamus. Itaque ut nisi laborum magni labore blanditiis? Assumenda soluta amet iusto! Nisi aspernatur illo obcaecati et iusto unde corporis exercitationem commodi amet, sunt voluptates soluta eaque omnis eveniet similique magni quae cumque id accusantium vel minima nobis est esse?</p> <p>Aspernatur, ullam perspiciatis aliquid qui quos nam expedita et recusandae eaque quae iste. Molestiae, quos quia! Officia autem non sapiente ullam architecto, tempora nisi ex temporibus laboriosam sed perspiciatis et impedit quasi repellat voluptatem repellendus. Impedit in molestiae excepturi, fugit cupiditate ex? Quaerat pariatur, eum maxime veritatis ipsum voluptate consequuntur id repudiandae odio nulla consequatur sed culpa, fugiat expedita tempora.</p>',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem, ipsum keempat',
+            'body' => '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore laudantium maxime veritatis vero, quaerat necessitatibus ratione voluptatum quod eveniet id, perferendis iure odit rem quis cum molestiae eaque quam sapiente adipisci ipsa dolore voluptas facere ipsum. Atque nihil vitae possimus esse, quis dignissimos sequi eaque modi officiis eligendi sunt in accusamus. Itaque ut nisi laborum magni labore blanditiis? Assumenda soluta amet iusto! Nisi aspernatur illo obcaecati et iusto unde corporis exercitationem commodi amet, sunt voluptates soluta eaque omnis eveniet similique magni quae cumque id accusantium vel minima nobis est esse?</p> <p>Aspernatur, ullam perspiciatis aliquid qui quos nam expedita et recusandae eaque quae iste. Molestiae, quos quia! Officia autem non sapiente ullam architecto, tempora nisi ex temporibus laboriosam sed perspiciatis et impedit quasi repellat voluptatem repellendus. Impedit in molestiae excepturi, fugit cupiditate ex? Quaerat pariatur, eum maxime veritatis ipsum voluptate consequuntur id repudiandae odio nulla consequatur sed culpa, fugiat expedita tempora.</p>',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }

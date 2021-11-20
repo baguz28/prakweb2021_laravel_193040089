b031e46b9cb3fd612fd675cf40fe6010d8ce8be7
baguz28 <bagussyahdewa28@gmail.com>
2021-11-20 21:45:10 +0700

    video 18 masih error

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
@@ -49,6 +50,15 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 
 }
